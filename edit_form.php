851c5665d74cf1a678366824bd685a15d21d50a6
Egraz Marlene <marlene.egraz@gmail.com>
2017-07-14 21:15:31 +0200

    design pg edit-form pr modif un post

--- edit_form.php
+++ edit_form.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
-
-<html>
+<html  lang="fr">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">  	
+        <meta name="viewport" content="width=device-width, user-scalable=yes" /><!--user-scalable=yes” sert à indiquer que l’utilisateur peut zoomer sur le contenu-->
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
         <title></title>
+
+        <style type="text/css">
+            body{
+                background-color: #d5dac1;
+                margin-top: 20px;
+            }
+        </style>
+
     </head>
-    <body>
+    <body class="container">
         <?php
         include_once './DataBase.php';
         include_once './User.php';
@@ -23,14 +35,15 @@
                 <form action="edit.php" method="POST">
                     <label for="title">Titre</label>
                     <input type="text" name="title" value="' . $title . '"/>
-                    <input type="hidden" name="ancienTitre" value="' . $title . '"/>
-                    <label for="description">Description</label>
-                    <textarea cols="50" rows="10" name="description" >' . $description . '</textarea>
-                    <label for="price">Prix</label>
+                    <br/><input type="hidden" name="ancienTitre" value="' . $title . '"/><br/>
+                    <label for="description">Description</label><br/>
+                    <textarea cols="50" rows="10" name="description" >' . $description . '</textarea><br/>
+                    <br/><label for="price">Prix</label>
                     <input type="number" name="price" value="' . $price . '"/> €
+                    <br/><br/>
                     <label for="photo">Photo</label>
-                    <input type="file"name="photo"/>
-                    <input type="submit" value="Envoyer" name="editpost"/>
+                    <input type="file"name="photo"/><br/>
+                    <br/><input type="submit" class="btn btn-primary" value="Envoyer" name="editpost"/>
                 </form>';
         }
         ?>
